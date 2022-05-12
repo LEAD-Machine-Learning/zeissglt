9351fae601b824c1abee985b8a9ab65f8125df84
fthiem <felix.thiem@lead.berlin>
2022-05-12 20:10:10 +0200

    updated installation file

--- installation.sh
+++ installation.sh
@@ -1,6 +1,17 @@
 sudo apt-get update
+
 sudo curl -kL dexterindustries.com/update_grovepi | bash
-sudo apt-get install python3-matplotlibsudo apt-get install python3-scipysudo pip3 install --upgrade pipsudo pip3 install jupyter
-sudo pip install psycopg2
+
+sudo apt-get install python3-matplotlib
+
+sudo apt-get install python3-scipy
+
+sudo pip install --upgrade pip
+
+sudo pip install jupyter
+
+sudo pip install psycopg2-binary
+
 sudo pip install pandas
+
 sudo pip install numpy
