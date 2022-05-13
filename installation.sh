33231ffa991ecba64e2e86ceb4b904cf109c6583
fthiem <felix.thiem@lead.berlin>
2022-05-13 21:09:24 +0200

    changed order of installations

--- installation.sh
+++ installation.sh
@@ -6,12 +6,14 @@ sudo apt-get install python3-matplotlib
 
 sudo apt-get install python3-scipy
 
-sudo pip install --upgrade pip
+sudo pip3 install --upgrade pip
 
-sudo pip install jupyter
+sudo pip3 install jupyter
 
 sudo apt install python3-psycopg2
 
-sudo pip install pandas
+sudo pip3 install pandas
 
-sudo pip install numpy
+sudo pip3 install numpy
+
+reboot
